b5ee1e75bfe8b71a402efb4eb38c158061f47df0
windzhou <312658243@qq.com>
2020-10-14 16:38:14 +0800

    kkkk

--- KdDjango/KdDjango/KdDjango/settings.py
+++ KdDjango/KdDjango/KdDjango/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'KdDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tcytest',
+        'HOST': '81.71.139.152',
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD': '123456'
     }
 }
 
--- KdDjango/KdDjango/kdapp/admin.py
+++ KdDjango/KdDjango/kdapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from kdapp.models import Test
+ 
 # Register your models here.
+admin.site.register(Test)
+
