bc9ed04f66299bf230dfdc1cbf9cdfc63e5670e2
wind88zhou <312658243@qq.com>
2020-10-26 16:51:43 +0800

    修改数据模型在admin的展示

--- KdDjango/KdDjango/kdapp/admin.py
+++ KdDjango/KdDjango/kdapp/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from kdapp.models import Test
- 
+
+
+class  ATestAdmin(admin.ModelAdmin):
+    list_display = ['id','name']
+
 # Register your models here.
-admin.site.register(Test)
+admin.site.register(Test,ATestAdmin)
 
