624e555f00eb5796573256b4a919b0e427c747ea
wind88zhou <312658243@qq.com>
2020-10-28 15:20:17 +0800

    注册2个新增的数据库表

--- KdDjango/KdDjango/kdapp/admin.py
+++ KdDjango/KdDjango/kdapp/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from kdapp.models import Test
+from kdapp.models import Test,BookInfo,HeroInfo
 
 
 class  ATestAdmin(admin.ModelAdmin):
     list_display = ['id','name']
 
 # Register your models here.
-admin.site.register(Test,ATestAdmin)
+admin.site.register(Test,ATestAdmin,BookInfo,HeroInfo)
 
