7b17f56addb73f638f9836f464fba4d1ebc73329
wind88zhou <312658243@qq.com>
2020-10-28 14:31:40 +0800

    add class BookInfo and class HeroInfo

--- KdDjango/KdDjango/kdapp/models.py
+++ KdDjango/KdDjango/kdapp/models.py
@@ -8,3 +8,19 @@ class Test(models.Model):
     # 魔法属性，返回数据名称
     def __str__(self):
         return self.name
+
+class BookInfo(models.Model):
+    '''图书模型类'''
+    btitle = models.CharField(max_length=20)
+    bpub_data = models.DateField()
+    bread = models.IntegeField(default=0)
+    bcomment = models.IntegerField(default=0)
+    isDelete = models.BooleanField(default=False)
+
+class HeroInfo(models.Model):
+    hname = models.CharField(max_length=20)
+    hgender = models.BooleanField(default=False)
+    hcomment = models.CharField(max_length=20)
+    # 关系属性
+    hbook = models.Foreignkey('BookInfo')
+    isDelete = models.BooleanField(default=False)
