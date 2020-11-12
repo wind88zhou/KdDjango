f06bbd866db7545683b5c1913743fe026b4413ce
wind88zhou <312658243@qq.com>
2020-11-12 17:28:25 +0800

    新增url反向解析页面

--- KdDjango/KdDjango/kdapp/views.py
+++ KdDjango/KdDjango/kdapp/views.py
@@ -143,3 +143,6 @@ def temp_inherit(request):
 # html转义
 def html_escape(request):
     return render(request,'html_escape.html',{'content':'<h1>hello</h1>'})
+
+def url_reverse(request):
+    return render(request,'url_reverse.html')
