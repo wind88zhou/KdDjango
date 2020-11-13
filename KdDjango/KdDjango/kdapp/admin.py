87315678b1acd6a138420bd403db66d2be0ad4c4
wind88zhou <312658243@qq.com>
2020-11-13 14:11:05 +0800

    111

--- KdDjango/KdDjango/kdapp/admin.py
+++ KdDjango/KdDjango/kdapp/admin.py
@@ -5,11 +5,12 @@ from kdapp.models import Test,BookInfo,HeroInfo
 class  ATestAdmin(admin.ModelAdmin):
     list_display = ['id','name']
 
+class BookInfoAdmin(admin.ModelAdmin):
+    list_per_page = 5 # 指定每页显示5条数据
+
 # Register your models here.
 admin.site.register(Test,ATestAdmin)
 admin.site.register([BookInfo,HeroInfo])
 
 admin.site.register(BookInfo,BookInfoAdmin)
 
-class BookInfoAdmin(admin.ModelAdmin):
-    list_per_page = 5 # 指定每页显示5条数据
