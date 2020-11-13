5bbf4600415691101467d5bbef81b6f7bf8b84cd
wind88zhou <312658243@qq.com>
2020-11-13 14:58:04 +0800

    移动操作栏在上面

--- KdDjango/KdDjango/kdapp/admin.py
+++ KdDjango/KdDjango/kdapp/admin.py
@@ -9,8 +9,8 @@ class BookInfoAdmin(admin.ModelAdmin):
     list_per_page = 5 # 指定每页显示5条数据
     list_display = ['id','btitle','new_title','bpub_data','bread','bcomment','isDelete']
     # 操作栏显示在下面
-    actions_on_bottom = True
-    actions_on_top = False
+    actions_on_bottom = False # 显示在下面则设置为True
+    actions_on_top = True # 显示在上面则设置为True
     # 新增右侧过滤栏
     list_filter = ['btitle']
     # 新增搜索功能 - 按btitle搜索
