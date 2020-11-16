310d0668a6881365c58df0dcabe8c00b6f53ab38
wind88zhou <312658243@qq.com>
2020-11-16 16:33:27 +0800

    11

--- KdDjango/KdDjango/kdapp/views.py
+++ KdDjango/KdDjango/kdapp/views.py
@@ -274,6 +274,6 @@ def post_request(lujing,data = {}):
 
 
 def api_request(request):
-    res = request.GET('http://81.71.139.152:8000/kdapp/showAppMsg')
+    res = request.GET.get('http://81.71.139.152:8000/kdapp/showAppMsg')
     print(res.text)
     return HttpResponse("OK")
